ba47f53af9d36713c79d18e14b9d8ef8ec983daa
ibrahimaberete <ibrahima.berete18@gmail.com>
2022-09-25 15:58:55 +0200

    Completed weather app

---
+++ src/api.js
@@ -0,0 +1,13 @@
+ export    const geoApiOptions = {
+	method: 'GET',
+	headers: {
+		'X-RapidAPI-Key': 'cc1ee442a4mshb733676f7e2db82p1f4548jsn2a69e73a6e88',
+		'X-RapidAPI-Host': 'wft-geo-db.p.rapidapi.com'
+	}
+};
+
+
+export const GEO_API_URL = "https://wft-geo-db.p.rapidapi.com/v1/geo";
+export const WEATHER_API_URL = "https://api.openweathermap.org/data/2.5";
+export const WEATHER_API_KEY = "0b0563a7663faf5c787f94c53b645547";
+
